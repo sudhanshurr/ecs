2465700b29af620cc4556adb59b0c9c48e16d88e
root <sudhanshurr@gmail.com>
2018-10-05 17:19:02 +0530

    edbedn

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,5 +1,8 @@
 
 #!/bin/bash
-	sudo apt-get update -y && sudo apt-get install awscli -y
-	sudo apt-get update -y
-sudo apt-get install python-setuptools python-dev build-essential
+sudo docker-compose build
+sudo docker-compose up
+sudo $(aws ecr get-login --no-include-email --region us-west-2)
+docker build -t test-ecs .
+docker tag test-ecs:latest 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
+docker push 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
