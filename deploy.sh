e2aadb603dd31ed3a601f49ac154c34d0bdb3a82
root <sudhanshurr@gmail.com>
2018-10-05 17:20:02 +0530

    debden

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-sudo docker-compose build
-sudo docker-compose up
-sudo $(aws ecr get-login --no-include-email --region us-west-2)
-docker build -t test-ecs .
-docker tag test-ecs:latest 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
-docker push 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
