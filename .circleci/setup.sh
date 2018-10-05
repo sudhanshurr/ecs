bd0d11f77c19d9b892aa2c00df146297568c31f0
root <sudhanshurr@gmail.com>
2018-10-05 17:24:14 +0530

    debden

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -3,6 +3,6 @@
 sudo docker-compose build
 sudo docker-compose up
 sudo $(aws ecr get-login --no-include-email --region us-west-2)
-docker build -t test-ecs .
-docker tag test-ecs:latest 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
-docker push 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
+sudo docker build -t test-ecs .
+sudo docker tag test-ecs:latest 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
+sudo docker push 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
