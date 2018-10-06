e3097fbfcea96c772f48948e05d50d2b7b2a8cd7
root <sudhanshurr@gmail.com>
2018-10-06 11:43:03 +0530

    edenned

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo chown -R root
 sudo docker-compose build
-sudo docker-compose up
 sudo $(aws ecr get-login --no-include-email --region us-west-2)
-sudo docker build -t test-ecs .
 sudo docker tag test-ecs:latest 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
 sudo docker push 617272699181.dkr.ecr.us-west-2.amazonaws.com/test-ecs:latest
